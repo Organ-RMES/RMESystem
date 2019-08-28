244193a8ca1a52d9050a7097f19befe17c85cc89
IceGodLi <627430778@qq.com>
2019-08-28 14:22:58 +0800

    123

--- RMES.Test/Program.cs
+++ RMES.Test/Program.cs
@@ -7,7 +7,7 @@ namespace RMES.Test
         static void Main(string[] args)
         {
 
-            Console.WriteLine("提交吧23");
+            Console.WriteLine("提交吧2355");
             Console.WriteLine("Hello World!");
         }
     }
