f2efc0fd2f7bab2edce821a51c05b630ca5b32d3
IceGodLi <627430778@qq.com>
2019-08-28 11:32:31 +0800

    123

---
+++ RMES.Test/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace RMES.Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
